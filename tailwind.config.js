e6208842eec4359e4fdfb16740d25187ca11bb60
Fabrício Maxwell <102274424+FabricioMaxwell@users.noreply.github.com>
2023-04-17 17:06:17 -0300

    Qr-code-component

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'White': 'hsl(0, 0%, 100%)',
+        'Light-gray': 'hsl(212, 45%, 89%)',
+        'Grayish-blue': 'hsl(220, 15%, 55%)',
+        'Dark-blue': 'hsl(218, 44%, 22%)'
+      },
+      fontFamily: {
+        'Outfit': ['Outfit', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
